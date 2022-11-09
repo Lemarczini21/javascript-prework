b3302efabc926b2e78f936126199a96d3e7463c1
Marcin Gwizdek <programowanie.kodilla@gmail.com>
2022-11-09 21:39:26 +0100

    add Odczytanie ruchu gracza

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
+// odczytanie ruchu komputera
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
+if(randomNumber == 1)   {
   computerMove = 'kamień';
 }   else if(randomNumber == 2)  {
     computerMove = 'papier';
@@ -13,3 +14,19 @@ if(randomNumber == 1){
 }
 printMessage('Mój ruch to: ' + computerMove);
 
+// odczytanie ruchu gracza
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1')  {
+  playerMove = 'kamień';
+}   else if(playerInput == '2') {
+    playerMove = 'papier';
+}   else    {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
